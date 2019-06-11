7727d9165d97b1290182e8d4a93409e8d541a9ff
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 16:18:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(int)
   x = 2
   prime_flag = true
   while x <= int / 2
-    if int % x == 0 || int % x <= -1
+    if int % x == 0 || int < 0
       prime_flag = false
       break
     end
